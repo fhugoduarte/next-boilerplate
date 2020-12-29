279fbaa457b79abac5d8fcb916947f05adcd6a7a
fhugoduarte <fhugoduarte@gmail.com>
2020-12-29 16:01:17 -0300

    fix: Tests coverage

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
   testEnvironment: "jsdom",
   testPathIgnorePatterns: ["/node_modules/", "/.next/"],
   collectCoverage: true,
-  collectCoverageFrom: ["src/**/*.ts(x)"],
-  setupFilesAfterEnv: ["<rootDir>/.jest/setup.ts"]
-}
+  collectCoverageFrom: ["src/**/*.ts(x)?", "!src/**/stories.tsx"],
+  setupFilesAfterEnv: ["<rootDir>/.jest/setup.ts"],
+};
